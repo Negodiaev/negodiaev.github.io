706b0137bd723cc0aef201dc1d7dd3bbd20232ba
Alex Negodiaev <alex.negodiaev@gmail.com>
2017-05-24 01:07:34 +0500

    Created a list of portfolio items

--- new/js/main.js
+++ new/js/main.js
@@ -19,5 +19,56 @@
     }
     //------------------------------------------------------------------------//
 
+
+    //------------------------------------------------------------------------//
+    //creating a projects list
+    var projectsContainer = document.getElementById('projects-container'),
+        projectsList = [],
+        projectsDir = 'http://negodiaev.github.io/new/projects/';
+
+    var request = new XMLHttpRequest();
+      request.open('GET', './../projects.json', true);
+
+      request.onload = function() {
+      	if (this.status >= 200 && this.status < 400) {
+      		var data = JSON.parse(this.response),
+              project = '';
+
+          for (var prop in data.projects) {
+            if (data.projects.hasOwnProperty(prop)) {
+              projectsList.push(data.projects[prop]);
+            }
+          }
+
+          projectsList.forEach(function(item) {
+            projectsContainer.innerHTML +=
+            '<div class="grid-25 tablet-grid-33 mobile-grid-50">' +
+              '<div class="project">' +
+                '<div class="project-image"' +
+                     'style="background-image: url(' +
+                     projectsDir + item.imageUrl + ');"></div>' +
+                '<div class="project-title">' +
+                  item.title +
+                '</div>' +
+                '<div class="project-decription">' +
+                  item.description +
+                '</div>' +
+                '<a href="' + projectsDir + item.url + '"' +
+                   'class="project-link" target="_blank">Show</a>' +
+              '</div>' +
+            '</div>';
+          });
+      	} else {
+      		console.error('Response received and there was an error');
+      	}
+      };
+
+      request.onerror = function() {
+      	console.error('Request error');
+      };
+
+      request.send();
+    //------------------------------------------------------------------------//
+
   });
 }());
