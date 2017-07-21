94edffd34a9cdbf99dece6a4dd07f72447757bdd
Alex Negodiaev <alex.negodiaev@gmail.com>
2017-07-21 07:33:20 +0500

    Added lazy loading of projects and updated backgrounds

--- js/ga.js
+++ js/ga.js
@@ -1,8 +1,1 @@
-// Google Analytics script
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-39158717-1', 'auto');
-ga('send', 'pageview');
+!function(e,t,a,n,c,o,s){e.GoogleAnalyticsObject=c,e[c]=e[c]||function(){(e[c].q=e[c].q||[]).push(arguments)},e[c].l=1*new Date,o=t.createElement(a),s=t.getElementsByTagName(a)[0],o.async=1,o.src="https://www.google-analytics.com/analytics.js",s.parentNode.insertBefore(o,s)}(window,document,"script",0,"ga"),ga("create","UA-39158717-1","auto"),ga("send","pageview");
